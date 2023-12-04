f7e3d52dd36e530c608110f7c36ea0efee9b06e9
t0obUnR34L <khocvole1kg3@gmail.com>
2023-12-04 15:37:12 +0700

    fix

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const express = require('express');
 const path = require('path');
 const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
+const session = require('express-session');
+const SessionCookie = require('express-session-cookie');
 
 dotenv.config();
 
@@ -10,11 +12,22 @@ const { registerUser, loginUser } = require('./dataquery');
 
 const app = express();
 const port = 3000;
-const secretKey = process.env.SECRET_KEY;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
+app.use(session({
+    secret: process.env.SECRET_KEY,
+    resave: false,
+    saveUninitialized: true,
+}));
 
+app.use('/dashboard', SessionCookie({
+    sessionName: 'token',
+    sessionOptions: {
+        httpOnly: true,
+        secure: false,
+    },
+}));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'templates', 'main.html'));
 });
@@ -28,9 +41,17 @@ app.get('/register', (req, res) => {
 });
 
 app.get('/dashboard', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'templates', 'dashboard.html'));
+    if (req.session.user) {
+        res.sendFile(path.join(__dirname, 'public', 'templates', 'dashboard.html'));
+    } else {
+        res.redirect('/login');
+    }
 });
 
+app.get('/logout', (req, res) => {
+    req.session.destroy();
+    res.redirect('/');
+});
 app.post('/register', (req, res) => {
     if (!req.body.email) {
         return res.status(400).send('Vui lòng cung cấp địa chỉ email.');
@@ -56,7 +77,7 @@ app.post('/login', (req, res) => {
 
         if (user) {
             const token = jwt.sign({ username: user.username, role: "user" }, secretKey);
-            res.cookie('token', token);
+            req.session.user = user;
             res.redirect('/dashboard');
         } else {
             res.status(401).send('Tên đăng nhập hoặc mật khẩu không đúng!');
